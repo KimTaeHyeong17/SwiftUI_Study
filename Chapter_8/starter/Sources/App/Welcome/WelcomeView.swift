7376ef0447816ecebaf717a060ed4a50fe5345bb
김태형[NOW Dev] <taehyeong.kim96@navercorp.com>
2020-10-22 00:35:51 +0900

    chapter8

--- Chapter_8/starter/Sources/App/Welcome/WelcomeView.swift
+++ Chapter_8/starter/Sources/App/Welcome/WelcomeView.swift
@@ -33,17 +33,38 @@
 import SwiftUI
 
 struct WelcomeView: View {
-  var body: some View {
-    ZStack {
-      WelcomeBackgroundImage()
-      
-      WelcomeMessageView()
+    @EnvironmentObject var userManager : UserManager
+    @ObservedObject var challengesViewModel = ChallengesViewModel()
+    @State var showPractice = false
+    
+    var body: some View {
+        if showPractice {
+            // 2
+            PracticeView(
+                challengeTest: $challengesViewModel.currentChallenge,
+                userName: $userManager.profile.name )
+        }else{
+            ZStack {
+                WelcomeBackgroundImage()
+                VStack{
+                    Text(verbatim: "Hi, \(userManager.profile.name)")
+                    WelcomeMessageView()
+                    Button(action: { self.showPractice = true
+                    }, label: {
+                        HStack {
+                            Image(systemName: "play")
+                            Text(verbatim: "Start")
+                        }
+                    })
+                }
+            }
+        }
     }
-  }
 }
 
 struct WelcomeView_Previews: PreviewProvider {
-  static var previews: some View {
-    WelcomeView()
-  }
+    static var previews: some View {
+        WelcomeView()
+            .environmentObject(UserManager())
+    }
 }
