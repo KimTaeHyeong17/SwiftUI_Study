7228e3f3bd5ae223804c61fe0b4ecf68126bcbdc
김태형[NOW Dev] <taehyeong.kim96@navercorp.com>
2020-10-22 16:21:37 +0900

    chapter9

--- Chapter_9/starter/Sources/App/Practice/ChallengeView.swift
+++ Chapter_9/starter/Sources/App/Practice/ChallengeView.swift
@@ -33,37 +33,63 @@
 import SwiftUI
 
 struct ChallengeView: View {
-  let challengeTest: ChallengeTest
-  
-  @State var showAnswers = false
-
-  var body: some View {
-    VStack {
-      Button(action: {
-        self.showAnswers = !self.showAnswers
-      }) {
-        QuestionView(question: challengeTest.challenge.question)
-          .frame(height: 300)
-      }
-      
-      if showAnswers {
-        Divider()
-        ChoicesView(challengeTest: challengeTest)
-          .frame(height: 300)
-          .padding()
-      }
+    let challengeTest: ChallengeTest
+    
+    @State var showAnswers = false
+    @Binding var numberOfAnswered: Int
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+    @Environment(\.questionsPerSession) var questionPerSession
+    
+    // 1
+    @ViewBuilder
+    var body: some View {
+        // 2
+        if verticalSizeClass == .compact { // 3
+            VStack { // 4
+                HStack {
+                    Button(action: {self.showAnswers = !self.showAnswers }) {
+                        QuestionView(question: challengeTest.challenge.question)
+                    }
+                    if showAnswers {
+                        Divider()
+                        ChoicesView(challengeTest: challengeTest)
+                    }
+                }
+                ScoreView(
+                    numberOfQuestions: questionPerSession,
+                    numberOfAnswered: $numberOfAnswered
+                )
+            }
+        } else {
+            // 5
+            VStack {
+                Button(action: {self.showAnswers = !self.showAnswers }) {
+                    QuestionView(question: challengeTest.challenge.question) .frame(height: 300)
+                }
+                ScoreView(
+                    numberOfQuestions: questionPerSession,
+                    numberOfAnswered: $numberOfAnswered
+                )
+                if showAnswers {
+                    Divider()
+                    ChoicesView(challengeTest: challengeTest)
+                        .frame(height: 300)
+                        .padding() }
+            }
+        }
     }
-  }
+    
 }
 
 
 struct ChallengeView_Previews: PreviewProvider {
+    @State static var numberOfAnswered : Int = 0
     static let challengeTest = ChallengeTest(
-      challenge: Challenge(question: "おねがい　します", pronunciation: "Onegai shimasu", answer: "Please"),
-      answers: ["Thank you", "Hello", "Goodbye"]
+        challenge: Challenge(question: "おねがい　します", pronunciation: "Onegai shimasu", answer: "Please"),
+        answers: ["Thank you", "Hello", "Goodbye"]
     )
-
+    
     static var previews: some View {
-      return ChallengeView(challengeTest: challengeTest)
+        return ChallengeView(challengeTest: challengeTest, numberOfAnswered: $numberOfAnswered)
     }
 }
